576c2aa892c008b8bb28e1d61fc87009b54fa3b6
Jidoki-San <guiferreira8545@gmail.com>
2022-05-15 22:34:37 -0300

    .

---
+++ Aula_02/Course/Course/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            //sbyte or SByte, has possible values from  -128 to 127, so it accepts 1 byte
+            sbyte x = -100;
+
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Aula_02/Course_exemple02/Course_exemple02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Course_exemple02 {
+    class Program {
+        static void Main(string[] args) {
+
+            // byte values: 0 to 127 
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            //long for long numbers      L = indicates long number
+            long n4 = 215897065L;
+
+            //value modifier: variable++
+            n1++;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+        }
+    }
+}
