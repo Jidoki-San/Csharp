201c6fcaa07a89f9d2748f9e672713b53eb9c330
Jidoki-San <guiferreira8545@gmail.com>
2022-07-30 19:15:46 -0300

    1h 38min

---
+++ Atividades/Bakhara/Bakhara/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bakhara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Programa que cálcula a fórmula de Baskhara
+            // Sendo a formula de delta: b2 - 4 * a * c
+            // Formula x1 e x2 = -b +- Sqrt(delta) / 2 * a
+
+            double a = -2, b = 3, c = 6;
+
+            double delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            double x1 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(delta);
+            Console.WriteLine(x1.ToString("F2"));
+            Console.WriteLine(x2.ToString("F2"));
+            
+        }
+    }
+}
---
+++ Curso C# - 5H - Essencial/Variaveis/Variaveis/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Int - -156 to 32334
+            // Float - -15.6 to 232.132
+            // Bool - true or false
+            // String - "type text"
+            // Char -  unique character 'a'
+            // Var - define the variable
+
+            // A - Z
+            // 0 - 9 ==> only in the middle or end of the variable 
+            // @ $ . ==> prohibited
+
+            int data_WW2 = 1942;
+            string cor_favorita = "cinza";
+            float velocidade_F1 = 294.48f;
+            bool data_WW2_aconteceu = true;
+
+            Console.WriteLine(data_WW2);
+            Console.WriteLine(cor_favorita);
+            Console.WriteLine(velocidade_F1);
+            Console.WriteLine(data_WW2_aconteceu);
+
+            // Caso queira mudar o valores das variavei, não precisa colocar o comand na frente dele
+
+            velocidade_F1 = 543.55f;
+            cor_favorita = "azul";
+
+            Console.WriteLine(velocidade_F1);
+            Console.WriteLine(cor_favorita);
+        }
+    }
+}
